b613b198acbac1e7b07cafca0f749270a725da0e
electropicks <lkbidy@gmail.com>
2023-12-09 15:37:02 -0800

    make app functional again

--- src/main/java/org/codevillage/Main.java
+++ src/main/java/org/codevillage/Main.java
@@ -1,5 +1,6 @@
 package org.codevillage;
 
+import lombok.extern.log4j.Log4j2;
 import org.codevillage.fetching.DataFetcher;
 import org.codevillage.fetching.GithubDataFetcher;
 import org.codevillage.fetching.LocalDataFetcher;
@@ -23,6 +24,7 @@ import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
+@Log4j2
 public class Main extends JFrame {
   public static void main(String[] args) {
     Main window = new Main();
@@ -49,38 +51,36 @@ public class Main extends JFrame {
     // logic
     DataFetcher[] fetch = { null };
     fetch[0] = new GithubDataFetcher();
-    submitButton.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        String selectedDataType = (String) dataTypeDropdown.getSelectedItem();
-        String link = linkTextField.getText();
-        String targetPath = targetPathTextField.getText();
-        JOptionPane.showMessageDialog(Main.this,
-            "Selected Data Type: " + selectedDataType + "\nLink: " + link + "\nTarget Path: " + targetPath);
-        fetch[0].downloadPackage(link, targetPath);
-        File directory = new File(targetPath);
-        SourceCodeParser sourceCodeParser = new SourceCodeParser();
-        try {
-          ArrayList<JavaEntity> entities = sourceCodeParser.parseSourceFiles(directory);
-          NeighborhoodGroupingLink neighborhoodGroupingLink = new NeighborhoodGroupingLink(
-                  new InterfaceInsertLink(
-                          new ShapeInitLink(
-                                  new ShapesRelativePositioningLink(
-                                          new NeighborhoodDimensionsLink(
-                                                  new NeighborhoodAbsolutePositioningLink(
-                                                          new ShapesAbsolutePositioningLink(
-                                                                  new PutShapesChainEnd()
-                                                          )
-                                                  )
-                                          )
-                                  )
-                          )
-                  )
-          );
-          neighborhoodGroupingLink.position(entities);
-        } catch (IOException ioError) {
-          ioError.printStackTrace();
-        }
+    submitButton.addActionListener(e -> {
+      String selectedDataType = (String) dataTypeDropdown.getSelectedItem();
+      String link = linkTextField.getText();
+      String targetPath = targetPathTextField.getText();
+      JOptionPane.showMessageDialog(Main.this,
+          "Selected Data Type: " + selectedDataType + "\nLink: " + link + "\nTarget Path: " + targetPath);
+      fetch[0].downloadPackage(link, targetPath);
+      File directory = new File(targetPath);
+      SourceCodeParser sourceCodeParser = new SourceCodeParser();
+      try {
+        ArrayList<JavaEntity> entities = sourceCodeParser.parseSourceFiles(directory);
+        NeighborhoodGroupingLink neighborhoodGroupingLink = new NeighborhoodGroupingLink(
+                new InterfaceInsertLink(
+                        new ShapeInitLink(
+                                new ShapesRelativePositioningLink(
+                                        new NeighborhoodDimensionsLink(
+                                                new NeighborhoodAbsolutePositioningLink(
+                                                        new ShapesAbsolutePositioningLink(
+                                                                new PutShapesChainEnd()
+                                                        )
+                                                )
+                                        )
+                                )
+                        )
+                )
+        );
+        neighborhoodGroupingLink.position(entities);
+        log.info("Entities: " + entities);
+      } catch (IOException ioError) {
+        ioError.printStackTrace();
       }
     });
 
@@ -108,25 +108,17 @@ public class Main extends JFrame {
     JButton openGraphButton = new JButton("Open Graph Window");
     topPanel.add(openGraphButton);
 
-    openGraphButton.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        openGraphWindow();
-      }
-    });
+    openGraphButton.addActionListener(e -> openGraphWindow());
 
-    dataTypeDropdown.addItemListener(new ItemListener() {
-      @Override
-      public void itemStateChanged(ItemEvent e) {
-        if (e.getStateChange() == ItemEvent.SELECTED) {
-          String selectedDataType = (String) e.getItem();
-          if ("GitHub".equals(selectedDataType)) {
-            fetch[0] = new GithubDataFetcher();
-          } else if ("Subversion".equals(selectedDataType)) {
-            fetch[0] = new SVNDataFetcher();
-          } else {
-            fetch[0] = new LocalDataFetcher();
-          }
+    dataTypeDropdown.addItemListener(e -> {
+      if (e.getStateChange() == ItemEvent.SELECTED) {
+        String selectedDataType = (String) e.getItem();
+        if ("GitHub".equals(selectedDataType)) {
+          fetch[0] = new GithubDataFetcher();
+        } else if ("Subversion".equals(selectedDataType)) {
+          fetch[0] = new SVNDataFetcher();
+        } else {
+          fetch[0] = new LocalDataFetcher();
         }
       }
     });
